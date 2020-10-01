56e5d5d57c3f1d83b8150f6c3247c508595862a5
modmam18 <modmam18@htl-kaindorf.at>
2020-10-01 14:50:36 +0200

    UE16

--- 3ahme/NetBeansProjects/ue16_RechnenMitBruechen/main.c
+++ 3ahme/NetBeansProjects/ue16_RechnenMitBruechen/main.c
@@ -39,14 +39,66 @@ void gibMenueAus() {
   //Menüfunktion auswählen 
 }
 
+void addiereBrueche(int z1, int n1, int z2, int n2, int *ze, int *ne) {
+    
+    *ze = z1 * n1 + z2 * n2;
+    *ne = n1 * n2;
+}
+
+void subtrahiereBrueche(int z1, int n1, int z2, int n2, int *ze, int *ne) {
+    
+    *ze = z1 * n1 - z2 * n2;
+    *ne = n1 * n2;
+}
+
+int ggT(int n, int z) {
+    
+    if (n < 0) {
+        n = -n;
+    }
+    if (z < 0) {
+        z = -z;
+    }
+    if (n == 0 || z == 0) {
+        return 1;
+    }
+    
+    while (z != n) {
+        if ( z > n) {
+            z = z - n;
+        } else {
+            n = n - z;
+        }
+    }
+    return n; 
+}
+
+void kuerzen(int *ze, int *ne) {
+    
+    int teiler = ggT(*ze, *ne);
+    *ze = *ze / teiler;
+    *ne = *ne / teiler;
+}
+
+void gibBruchAus(char *txt, int ze, int ne) {
+    
+    printf("%s:\n", txt);
+    kuerzen(&ze, &ne);
+    printf("%d / %d\n", ze, ne);
+}
+
+
 int main ()
 {
   int wahl = 0;
   char s[4]; 
   int z1, z2, n1, n2;
+  int ze = 0;
+  int ne = 0;
   do {
     gibMenueAus();
     //Menüfunktion auswählen
+   
     
     wahl = gibGanzeZahlEin("Waehle die Funktion (1-4)");
     switch(wahl) {
@@ -54,12 +106,16 @@ int main ()
       printf("Addieren\n");
       gibBruchEin("Bruch 1", &z1, &n1);
       gibBruchEin("Bruch 2", &z2, &n2);
+      addiereBrueche(z1, n1, z2, n2,&ze, &ne);
+      gibBruchAus("Ergebnis", ze, ne);
       break;
     
     case 2: //Subtrahieren
       printf("Subtrahieren\n");
       gibBruchEin("Bruch 1", &z1, &n1);
       gibBruchEin("Bruch 2", &z2, &n2);
+      subtrahiereBrueche(z1, n1, z2, n2, &ze, &ne);
+      gibBruchAus("Ergebnis", ze, ne);
       break;
       
     case 3: //Anzahl der Stellen
--- 3ahme/NetBeansProjects/ue16_RechnenMitBruechen/nbproject/Makefile-impl.mk
+++ 3ahme/NetBeansProjects/ue16_RechnenMitBruechen/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=ue16_rechnenMitBruechen
+PROJECTNAME=ue16_RechnenMitBruechen
 
 # Active Configuration
 DEFAULTCONF=Debug
